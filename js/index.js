999ca4ca5d28ddde29ef167cc8cc7e862a959cf6
Vinicius <viniciusr.camargo@gmail.com>
2023-11-02 19:11:08 -0300

    Conclusão do Projeto Fokus

--- js/index.js
+++ js/index.js
@@ -30,21 +30,25 @@ mudicaFocoInput.addEventListener('change', () => {
 });
 
 focoBt.addEventListener('click', () => {
+   tempoDecorridoEmSegundos = 1500;
    alterarContexto('foco');
    focoBt.classList.add('active');
 })
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300;
     alterarContexto('descanso-curto');
     curtoBt.classList.add('active');
 })
 
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900;
     alterarContexto('descanso-longo');
     longoBt.classList.add('active');
 })
 
 function alterarContexto(contexto){
+    mostrarTempo();
     botoes.forEach(function(contexto){
         contexto.classList.remove('active');
     });
@@ -69,7 +73,7 @@ function alterarContexto(contexto){
 
 const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0){
-        // stop.play();
+        stop.play();
         alert('Tempo finalizado!')
         zerar();
         return;
@@ -100,8 +104,12 @@ function zerar(){
 }
 
 function mostrarTempo(){
-    const tempo = tempoDecorridoEmSegundos;
-    tempoNaTela.innerHTML = `${tempo}`;
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-BR', {
+        minute: '2-digit',
+        second: '2-digit',
+    });
+    tempoNaTela.innerHTML = `${tempoFormatado}`;
 }
 
 mostrarTempo();

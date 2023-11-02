69e245bbd80092d9a6aef7c94786531a13ae7f98
Vinicius <viniciusr.camargo@gmail.com>
2023-11-02 19:00:58 -0300

    Implementando variáveis e manipulando elementos do DOM

--- js/index.js
+++ js/index.js
@@ -6,6 +6,28 @@ const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
+const startPauseBt = document.querySelector('#start-pause');
+const mudicaFocoInput = document.querySelector('#alternar-musica');
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
+const alterarImagemPlauPause = document.querySelector('#start-pause img');
+const tempoNaTela = document.querySelector('#timer');
+
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const tocar = new Audio('/sons/play.wav');
+const pause = new Audio('/sons/pause.mp3');
+const stop = new Audio('/sons/beep.mp3');
+
+musica.loop = true;
+let tempoDecorridoEmSegundos = 1500;
+let intervaloId = null;
+
+mudicaFocoInput.addEventListener('change', () => {
+    if(mudicaFocoInput.checked){
+        musica.play();
+    }else{
+        musica.pause();
+    }
+});
 
 focoBt.addEventListener('click', () => {
    alterarContexto('foco');
@@ -44,3 +66,42 @@ function alterarContexto(contexto){
             break;
     }
 }
+
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <= 0){
+        // stop.play();
+        alert('Tempo finalizado!')
+        zerar();
+        return;
+    }
+    tempoDecorridoEmSegundos -= 1;
+    mostrarTempo();
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar(){
+    if(intervaloId){
+        pause.play();
+        zerar();
+        return;
+    }
+    tocar.play();
+    intervaloId = setInterval(contagemRegressiva, 1000);
+    alterarImagemPlauPause.setAttribute('src', 'imagens/pause.png');
+    iniciarOuPausarBt.textContent = 'Pausar';
+}
+
+function zerar(){
+    clearInterval(intervaloId);
+    iniciarOuPausarBt.textContent = 'Começar';
+    alterarImagemPlauPause.setAttribute('src', 'imagens/play_arrow.png');
+    intervaloId = null;
+}
+
+function mostrarTempo(){
+    const tempo = tempoDecorridoEmSegundos;
+    tempoNaTela.innerHTML = `${tempo}`;
+}
+
+mostrarTempo();
